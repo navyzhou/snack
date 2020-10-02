514df38dfa915e7a9750768c2f755576fe760fb1
navyzhou <qfxsxhfy@126.com>
2020-10-02 11:46:10 +0800

    配置中心

---
+++ snack-eureka-server/src/main/java/com/yc/snack/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.yc.snack.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	// 高版本spring cloud会默认开启csrf功能  跨域访问
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/eureka/**");
+	}
+}
